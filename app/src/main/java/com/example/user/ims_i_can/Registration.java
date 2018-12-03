e02d20bd374cf77a62640f25ed3acf5aefa20692
Sparks <jigar21192@gmail.com>
2018-12-03 10:23:21 +0530

    123

--- app/src/main/java/com/example/user/ims_i_can/Registration.java
+++ app/src/main/java/com/example/user/ims_i_can/Registration.java
@@ -1,5 +1,6 @@
 package com.example.user.ims_i_can;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.location.Address;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +9,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Registration extends AppCompatActivity {
     Button Btn;
     EditText Fname, Lname, EnrNo, Bdate, Email, Mobno, Pmobno, Address;
     Spinner State, City, Area;
     RadioButton Rmale, Rfemale;
+    RadioGroup Rgroup;
+    DatePickerDialog.OnDateSetListener date;
+    Calendar myCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,17 @@ public class Registration extends AppCompatActivity {
         Address = (EditText) findViewById(R.id.Address);
         Email = (EditText) findViewById(R.id.Email);
 
+        Bdate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(Registration.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        datepick();
 
         Btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +75,29 @@ public class Registration extends AppCompatActivity {
         });
     }
 
+    private void datepick() {
+        myCalendar = Calendar.getInstance();
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+
+        };
+    }
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        Bdate.setText(sdf.format(myCalendar.getTime()));
+    }
 
     private boolean validate(EditText editText) {
 // check whether the field is empty or not
