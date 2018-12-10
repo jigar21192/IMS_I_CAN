7f6092f0fb37f8e01b288c6550f3d4362e97b25d
Sparks <jigar21192@gmail.com>
2018-12-10 12:02:19 +0530

    123

--- app/src/main/java/com/example/user/ims_i_can/Ranker_list.java
+++ app/src/main/java/com/example/user/ims_i_can/Ranker_list.java
@@ -1,15 +1,16 @@
 package com.example.user.ims_i_can;
 
 import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+
 import android.view.View;
 import android.widget.Button;
 
 public class Ranker_list extends AppCompatActivity {
     Button Signup,Signin;
-    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,9 @@ public class Ranker_list extends AppCompatActivity {
         setContentView(R.layout.activity_ranker_list);
         Signup=(Button)findViewById(R.id.Signup);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar_rankerlist);
-        // Sets the Toolbar to act as the ActionBar for this Activity window.
-        // Make sure the toolbar exists in the activity and is not null
-        setSupportActionBar(toolbar);
 
-        getSupportActionBar().setTitle("Hi");
+
+
 
         Signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,4 +41,6 @@ public class Ranker_list extends AppCompatActivity {
 
 
     }
+
+
 }
